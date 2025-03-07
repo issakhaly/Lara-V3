e4d81161037943e1edd3bdd377d4fd6a8978cdb7
issakhaly <157059290+issakhaly@users.noreply.github.com>
2025-03-07 16:15:26 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,19 +5,19 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "P6JnxTQQ#AWCpwU6lDaxWB38tOn-I3IsgO0DdF14vcCCPKCxyHvE", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "XjJThKRC#eLpXr1EO1ErKAhq1iEzT163bN4w2q4gqLyuaqapp7gM", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`AUTO STATUS JUST NOW SEEN BY LARA MD`_*",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "prive",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",   
+ANTI_BAD: process.env.ANTI_BAD || "false",   
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
